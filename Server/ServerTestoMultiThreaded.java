667a7340b6b781680c8fbae42f4994cb5ce32c08
Lorenzo-Orlando-JCMaxwell-5Ai <lorenzo.orlando1@jcmaxwell.it>
2016-12-07 09:23:08 +0100

    soketWorkerUpdate
    Modifiche al socketWorker per salvare i nomi dei client

--- Server/ServerTestoMultiThreaded.java
+++ Server/ServerTestoMultiThreaded.java
@@ -1,49 +1,48 @@
-/*
- * ServerTesto MultiThreaded.java Server che attende per richieste di connessioni da Clients
- * e li gestisce in modo contemporaneo generando un socket "worker" per ogni connessione.
- * 
- */
-
-/**
- *
- * @author Prof. Matteo Palitto
- */
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ServerTestoMultiThreaded {
-
-    public static void main(String[] args) {
-
-        if (args.length != 1) {
+public class ServerTestoMultiThreaded 
+{
+    public static void main(String[] args) 
+    {
+        if (args.length != 1) 
+        {
             System.out.println("Uso: java ServerTestoMultithreaded <Porta Server>");
             return;
         }
 
         int portNumber = Integer.parseInt(args[0]);
 
-
-        try{
+        try
+        {
             ServerSocket server = new ServerSocket(portNumber);
             System.out.println("Server di Testo in esecuzione...  (CTRL-C quits)\n");
-
-            while(true){
+            while(true)
+            {
                 SocketWorker w;
-                try {
+                try 
+                {   
                     //server.accept returns a client connection
+                    List<String> utenti= new ArrayList();
                     w = new SocketWorker(server.accept());
                     Thread t = new Thread(w);
                     t.start();
-                } catch (IOException e) {
+                } 
+                
+                catch (IOException e) 
+                {
                     System.out.println("Connessione NON riuscita con client: ");
                     System.exit(-1);
                 }
             }
-        } catch (IOException e) {
+        } 
+        
+        catch (IOException e) 
+        {
             System.out.println("Error! Porta: " + portNumber + " non disponibile");
             System.exit(-1);
         }
-
-        
     }
 }
--- Server/socketWorker.java
+++ Server/socketWorker.java
@@ -1,56 +1,81 @@
-/*
- * socketWorker.java ha il compito di gestire la connessione al socket da parte di un Client.
- * Elabora il testo ricevuto che in questo caso viene semplicemente mandato indietro con l'aggiunta 
- * di una indicazione che e' il testo che viene dal Server.
- */
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Prof. Matteo Palitto
- */
 class SocketWorker implements Runnable {
-  private Socket client;
-
+    private Socket client;
+    
+    
     //Constructor: inizializza le variabili
-    SocketWorker(Socket client) {
+    SocketWorker(Socket client) 
+    {
         this.client = client;
         System.out.println("Connesso con: " + client);
     }
-
+    
     // Questa e' la funzione che viene lanciata quando il nuovo "Thread" viene generato
-    public void run(){
-        
+    public void run()
+    {
         BufferedReader in = null;
         PrintWriter out = null;
-        try{
+        String line = "";
+        String nomeHost = "";
+        List<String> utenti= new ArrayList();
+        try
+        {
           // connessione con il socket per ricevere (in) e mandare(out) il testo
           in = new BufferedReader(new InputStreamReader(client.getInputStream()));
           out = new PrintWriter(client.getOutputStream(), true);
-        } catch (IOException e) {
+        }
+        
+        catch (IOException e) 
+        {
           System.out.println("Errore: in|out fallito");
           System.exit(-1);
         }
-
-        String line = "";
+        
         int clientPort = client.getPort(); //il "nome" del mittente (client)
         while(line != null){
-          try{
+          try
+          {
+            //se il nome nella variabile non c'è lo inserisce
+            if(nomeHost == "")
+            {
+                out.println("Scrivi il comando !listautenti per sapere chi è connesso");
+                out.println("Inserisci il nickname");
+                nomeHost = in.readLine();
+                utenti.add(nomeHost);
+            }
+            //inserisce il testo se il testo e il comando il comando verrà eseguito
             line = in.readLine();
+            if(line.equals("!listautenti"));
+            {
+                for(int j=0; j<utenti.size(); j++)
+                {
+                    out.println(utenti.get(j));
+                }
+            }
+            
             //Manda lo stesso messaggio appena ricevuto con in aggiunta il "nome" del client
-            out.println("Server-->" + clientPort + ">> " + line);
+            out.println("Server-->" + nomeHost + ">> " + line);
             //scrivi messaggio ricevuto su terminale
-            System.out.println(clientPort + ">> " + line);
-           } catch (IOException e) {
+            System.out.println(nomeHost + ">> " + line);
+           }
+          catch (IOException e) 
+          {
             System.out.println("lettura da socket fallito");
             System.exit(-1);
            }
         }
-        try {
+        try 
+        {
             client.close();
             System.out.println("connessione con client: " + client + " terminata!");
-        } catch (IOException e) {
+        }
+        
+        catch (IOException e) 
+        {
             System.out.println("Errore connessione con client: " + client);
         }
     }
