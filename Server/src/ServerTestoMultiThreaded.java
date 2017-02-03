831d08a3e33047a94ff2905cfc51a9baa0ea5dbd
Lorenzo-Orlando-JCMaxwell-5Ai <lorenzo.orlando1@jcmaxwell.it>
2017-02-03 08:21:11 +0100

    Add files via upload

---
+++ Server/src/ServerTestoMultiThreaded.java
@@ -0,0 +1,58 @@
+/**
+ *
+ * @author LolZ, Samuele Pavone, Andrea Musso
+ */
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class ServerTestoMultiThreaded
+{
+    //Dichiarazione della lista dei nomi client e dei gruppi
+    public static List<String> listaClient = new ArrayList();
+    public static List<ChatGroup> gruppi = new ArrayList();
+    
+    public static void main(String[] args) 
+    {
+        if (args.length != 1) 
+        {
+            System.out.println("Uso: java ServerTestoMultithreaded <Porta Server>");
+            return;
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+        
+        try
+        {
+            ServerSocket server = new ServerSocket(portNumber);
+            System.out.println("Server di Testo in esecuzione...  (CTRL-C quits)\n");
+
+            while(true)
+            {
+                SocketWorker w;
+                try 
+                {
+                    //server.accept returns a client connection
+                    w = new SocketWorker(server.accept());
+                    Thread t = new Thread(w);
+                    t.start();
+                } 
+                catch (IOException e) 
+                {
+                    System.out.println("Connessione NON riuscita con client: ");
+                    System.exit(-1);
+                }
+            }
+        } 
+        catch (IOException e) 
+        {
+            System.out.println("Error! Porta: " + portNumber + " non disponibile");
+            System.exit(-1);
+        }
+    }
+    
+    public void reInvia()
+    {
+        
+    }
+}
